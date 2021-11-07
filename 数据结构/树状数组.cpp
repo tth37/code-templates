5d5e92e545bbe5a1e88767a667c3f6def41e6a0b
tth37 <xgpsthd0902@outlook.com>
2021-11-07 23:24:36 +0800

    添加树状数组

---
+++ 数据结构/树状数组.cpp
@@ -0,0 +1,18 @@
+// 树状数组（单点修改，前缀和查询）
+struct BIT {
+    ll c[MAXN];
+    // 在位置 p 上加 val
+    void modify(int p, ll val) {
+        for (; p <= n; p += p & (-p)) {
+            c[p] += val;
+        }
+    }
+    // 查询位置 p 的前缀和
+    ll query(int p) {
+        ll ret = 0;
+        for (; p > 0; p -= p & (-p)) {
+            ret += c[p];
+        }
+        return ret;
+    }
+} T;
